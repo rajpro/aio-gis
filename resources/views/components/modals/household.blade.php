d96e91381effecd085594d4de7073a1fd2883cc4
Rajesh Sardar <rajeshkumarsardar@gmail.com>
2025-05-29 02:50:47 +0000

    pdf

--- resources/views/components/modals/household.blade.php
+++ resources/views/components/modals/household.blade.php
@@ -1,93 +1,179 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lodha Document</title>
+    <meta charset="utf-8">
+    <title>PGT HH Report</title>
     <style>
-        .table {
-            font-family: Arial, Helvetica, sans-serif;
-            border-collapse: collapse;
-            width: 100%;
+        body {
+            font-family: DejaVu Sans, sans-serif;
+            font-size: 12px;
+            line-height: 1.4;
+            color: #000;
+            padding: 10px;
+            border: 1px solid #ccc;
         }
-        .table th, .table td {
-            border: 1px solid #ddd;
-            padding: 8px;
+        h1, h2, h3, h4, h5 {
+            margin: 0;
+            padding: 0;
+            font-weight: bold;
         }
-        .table tr:nth-child(even){background-color: #f2f2f2;}
-        .table th {
-            padding-top: 12px;
-            padding-bottom: 12px;
-            text-align: left;
-            background-color:#858585;
-            color: white;
+        .hh-section {
+            border: 1px solid #000;
+            padding: 5px;
+        }
+        table {
+        width: 100%;
+        border-collapse: collapse;
+    }
+    .table th,
+    .table td {
+        padding: 0.1rem;
+        vertical-align: top;
+        border-top: 1px solid #dee2e6;
+    }
+    .table thead th {
+        vertical-align: bottom;
+        border-bottom: 2px solid #dee2e6;
+    }
+    .table-bordered {
+        border: 1px solid #dee2e6;
+    }
+    .table-bordered th,
+    .table-bordered td {
+        border: 1px solid #dee2e6;
+    }
+    .page-break {
+        page-break-before: always;
+        margin-top: 100px;
+    }
+    .header {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            
+        }
+        .content {
+            margin-top: 100px;
+        }
+
+        @page {
+            margin: 30px 30px 30px;
         }
     </style>
 </head>
 <body>
-<div class="modal-body">
-    <table class="table table-bordered mb-0">
-        <thead>
-            <tr>
-                <th>GP Block</th>
-                <th>Village</th>
-                <th>Household ID</th>
-                <th>Surveyor Name</th>
-                <th>Surveyor Team</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>{{$data->block}}</td>
-                <td>{{$data->village}}</td>
-                <td>{{$data->hh_id}}</td>
-                <td>{{$data->surveyor_name}}</td>
-                <td>{{$data->team}}</td>
-            </tr>
-        </tbody>
-    </table>
+    <div class="header">
+        <h3>CONSERVATION & DEVELOPMENT PLAN (CCD)<br>FOR PARTICULARLY VULNERABLE TRIBAL GROUPS (PGTs)</h3>
+        <h3>LODHA DEVELOPMENT AGENCY, MORADA, MAYURBHANJ</h3>
+        <h5>REGISTRATION FORM OF PGTs – LODHA - MAYURBHANJ</h6>
+    </div>
 
-    <div class="row mt-4">
-        <div class="col-xl-4">
-            <p class="text-muted">Demographic</p>
-            <table class="table table-bordered mb-0">
-                <tbody>
-                    @foreach($demo_column as $k => $v)
-                    <tr>
-                        <th>{{$v}}</th>
-                        <td>{{!is_array($data->demographic->$v)?$data->demographic->$v:""}}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
+    <div class="content">
+        <table>
+            <tr>
+                <td>
+                    <table>
+                        <tr><td>Household No.</td><td>HH-JUAL-BHAD-01</td></tr>
+                        <tr><td>Surveyor Name</td><td>Laxmi Majhi</td></tr>
+                        <tr><td>Geo Coordinates</td><td>21.786313, 86.898436</td></tr>
+                    </table>
+                </td>
+                <td>
+                    <table>
+                        <tr><td>Survey Completion Date</td><td>15/04/2025</td></tr>
+                        <tr><td>Surveyor Team</td><td>Agragamee</td></tr>
+                        <tr><td>sdf</td><td></td></tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <div class="hh-section">
+            <h4>Household Head (HH) Details</h4>
+
+            <table>
+                <tr><td>Gram Panchayat Name</td><td class="value">JUALIBHANGA</td></tr>
+                <tr><td class="label">Village Name</td><td class="value">Bhadrasole</td></tr>
+                <tr><td class="label">Household Head (HH) Name</td><td class="value">CHAITA TUDU</td></tr>
+                <tr><td class="label">Age</td><td class="value">38</td></tr>
+                <tr><td class="label">PGT Group</td><td class="value">LODHA TRIBE</td></tr>
+                <tr><td class="label">HH Mobile Number</td><td class="value">6371234291</td></tr>
             </table>
         </div>
-        <div class="col-xl-4">
-            <p class="text-muted">Socio</p>
-            <table class="table table-bordered mb-0">
-                <tbody>
-                    @foreach($socio_column as $k => $v)
-                    <tr>
-                        <th>{{$v}}</th>
-                        <td>{{$data->socio->$v}}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+
+        <h3>Demographic, Crime, Vulnerability & Admin Details</h3>
+        <table class="table table-bordered">
+            <tr>
+                <td style="width:48%;">
+                    <table>
+                        <tr><td colspan="2"><h4>Admin & HH Contact Details</h4></td></tr>
+                        <tr><td class="label">Sarpanch</td><td class="value">Smt. Anjali Dungdung</td></tr>
+                        <tr><td class="label">Sarpanch Mob.</td><td class="value">9937204127</td></tr>
+                        <tr><td class="label">Block</td><td class="value">MORADA</td></tr>
+                        <tr><td class="label">Police Station</td><td class="value">MORADA</td></tr>
+                        <tr><td class="label">Admin Email</td><td class="value">anjali.dungdung@mayurbhanj</td></tr>
+                        <tr><td class="label">District</td><td class="value">Mayurbhanj</td></tr>
+                        <tr><td class="label">State</td><td class="value">Odisha</td></tr>
+                        <tr><td class="label">HH Mob.</td><td class="value">7978035450</td></tr>
+                        <tr><td class="label">HH Alt Contact</td><td class="value">7978035450</td></tr>
+                    </table>
+                </td>
+                <td style="width:20px;"></td>
+                <td>
+                    <table>
+                        <tr><td colspan="2"><h4>HH Demographic Details</h4></td></tr>
+                        <tr><td class="label">Total Family Size</td><td class="value">7</td></tr>
+                        <tr><td class="label">Highest Literacy Level</td><td class="value">Upper Primary</td></tr>
+                        <tr><td class="label">Primary Occupation</td><td class="value">Agriculture</td></tr>
+                        <tr><td class="label">Avg. PCI (Monthly)</td><td class="value">₹ 514.29</td></tr>
+                        <tr><td class="label">Land Ownership</td><td class="value">>3 acres</td></tr>
+                        <tr><td class="label">Land Use</td><td class="value">Cultivation</td></tr>
+                        <tr><td class="label">Govt. Support Received</td><td class="value">Skill Training, LABHA Scheme, PVTGDP, BPGY (PVTG)</td></tr>
+                        <tr><td class="label">Existing Debt</td><td class="value">Yes</td></tr>
+                        <tr><td class="label">Debt Reason</td><td class="value">Livelihood</td></tr>
+                        
+                    </table>
+                </td>
+            </tr>
+        </table>
+
+        <table class="table table-bordered">
+            <tr>
+                <td style="width:48%;">
+                    <table>
+                        <tr><td colspan="2"><h4>Criminal History/Reporting History</h4></td></tr>
+                        <tr><td class="label">Reported Crime (in past 1 year)?</td><td class="value">Smt. Anjali Dungdung</td></tr>
+                        <tr><td class="label">Type of Crime</td><td class="value">9937204127</td></tr>
+                        <tr><td class="label">Reported to Police?</td><td class="value">MORADA</td></tr>
+                        <tr><td class="label">Police Action</td><td class="value">MORADA</td></tr>
+                        <tr><td class="label">Any Family members in Jail?</td><td class="value">anjali.dungdung@mayurbhanj</td></tr>
+                        <tr><td class="label">Jailed Relation</td><td class="value">Mayurbhanj</td></tr>
+                    </table>
+                </td>
+                <td style="width:20px;"></td>
+                <td>
+                    <table>
+                        <tr><td colspan="2"><h4>Vulnerability Assessment</h4></td></tr>
+                        <tr><td class="label">Police Presence in Locality</td><td class="value">7</td></tr>
+                        <tr><td class="label">Feeling of Safety?</td><td class="value">Upper Primary</td></tr>
+                        <tr><td class="label">Vulnerability Flags </td><td class="value">Agriculture</td></tr>
+                        <tr><td class="label" rowspan="3">Avg. PCI (Monthly)</td><td class="value" rowspan="3">₹ 514.29</td></tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+        <div class="page-break"></div>
+
+        <div style="border: 1px solid #000; padding: 5px;">
+            <h4 style="text-align:center;">Declaration of the Surveyor</h4>
+            <p>I hereby declare that all the particulars furnished are correct. I understand that my association direct or indirect with any unlawful organization is forbidden. I am aware that any incorrect information may lead to cancellation of my position and any future association with the Department of Tribal Affairs.
+                <br><b>Date: 4/28/2025 9:45:00 PM</b></p>
         </div>
-        <div class="col-xl-4">
-            <p class="text-muted">Crime</p>
-            <table class="table table-bordered mb-0">
-                <tbody>
-                    @foreach($crime_column as $k => $v)
-                    <tr>
-                        <th>{{$v}}</th>
-                        <td>{{$data->crime->$v}}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div style="border: 1px solid #000; padding: 5px;">
+            <h4 style="text-align:center;">FOR OFFICE USE ONLY</h4>
+            <p>Certified that all information and documents produced in support of the above particulars have been verified and found correct.</p>
+            <p style="float:right"><b>Signature of the Head of the Department (with seal)</b></p>
         </div>
     </div>
-</div>
 </body>
 </html>
